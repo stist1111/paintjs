b823b111a2019806a6a2724850a5cae1ba71a52d
stist1111@naver.com <stist1111@naver.com>
2020-07-16 17:50:48 +0900

    어렵다..

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const eraserBtn = document.getElementById("jsEraser");
+const palette = document.getElementById("palette");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 500;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
+canvas.color = "white";
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
@@ -67,6 +70,7 @@ function handleModeClick() {
 function handleCanvasClick() {
   if (filling) {
     ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    canvas.color = ctx.fillStyle;
   }
 }
 
@@ -81,6 +85,17 @@ function handleSaveClick() {
   link.download = "PAINTJS";
   link.click();
 }
+
+function handleEraseClick() {
+  ctx.strokeStyle = canvas.color;
+}
+
+changeColor = (event) => {
+  console.log(event.target.value);
+  const color = event.target.value;
+  ctx.strokeStyle = "#" + color;
+  ctx.fillStyle = "#" + color;
+};
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
@@ -106,4 +121,12 @@ if (mode) {
 
 if (saveBtn) {
   saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if (eraserBtn) {
+  eraserBtn.addEventListener("click", handleEraseClick);
+}
+
+if (palette) {
+  palette.addEventListener("blur", changeColor);
 }
